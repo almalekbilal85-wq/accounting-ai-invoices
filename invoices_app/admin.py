a9178f82ee3e1192fbd06c70f762560ac3695595
almalekbilal85-wq <almalekbilal85@gmail.com>
2026-05-18 15:24:26 +0200

    Update the models

--- invoices_app/admin.py
+++ invoices_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from invoices_app.models import Customer, Invoice, AccountingLine
+from invoices_app.models import Customer, Invoice, AccountingLine, Journal
 # Register your models here.
 
 admin.site.register(Customer)
+admin.site.register(Journal)
 admin.site.register(Invoice)
 admin.site.register(AccountingLine)
--- invoices_app/models.py
+++ invoices_app/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.urls import reverse
 
+from django.db.models import F
+from django.db import transaction
 # Create your models here.
 
 class Customer(models.Model):
     name = models.CharField(max_length=256)
     org_number = models.CharField(max_length=256)
     address = models.CharField(max_length=256, blank=True, null=True)
+    next_journal_number = models.PositiveIntegerField(default=1)
     
 
     def __str__(self):
@@ -15,17 +18,50 @@ class Customer(models.Model):
     def get_absolute_url(self):
         return reverse("invoices_app:customer_detail", kwargs={"pk": self.pk})
 
+class Journal(models.Model):
+    customer = models.ForeignKey(Customer, related_name='journals', on_delete=models.CASCADE)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    entry_date = models.DateField(blank=True, null=True)
+    journal_number = models.PositiveIntegerField(editable=False, blank=True, null=True)
+    vat_code = models.CharField(max_length=10, blank=True)
+
+    TYPE_CHOICES = [
+        ("manual", "Manual"),
+        ("invoice", "Invoice"),
+        ("bank", "Bank transaction"),
+    ]
+
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES, default="invoice")
+
+
+    def save(self, *args, **kwargs):
+        if self.journal_number is None:
+            with transaction.atomic():
+                customer = Customer.objects.select_for_update().get(pk=self.customer.pk)
+                self.journal_number = customer.next_journal_number
+                customer.next_journal_number = F("next_journal_number") + 1
+                customer.save()
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        jounal_str = self.customer.name + ' &&& V-'
+        if self.journal_number is not None:
+            jounal_str = jounal_str + str(self.journal_number)
+
+        return jounal_str
+
 class Invoice(models.Model):
-    customer = models.ForeignKey(Customer, related_name='invoices', on_delete=models.CASCADE)
+
+    journal = models.ForeignKey(Journal, on_delete=models.CASCADE, null=True, blank=True)
     invoice_number = models.CharField(max_length=256, blank=True, null=True)
     invoice_date = models.DateField(blank=True, null=True)
     due_date = models.DateField(blank=True, null=True)
     total_amount = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
     vat_amount = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
     currency = models.CharField(max_length=3,default="SEK", blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     supplier = models.CharField(max_length=256, blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
 
     file = models.FileField(
         upload_to="invoices/",
@@ -39,10 +75,11 @@ class Invoice(models.Model):
 
 
 class AccountingLine(models.Model):
-    invoice = models.ForeignKey(Invoice, related_name='accounting_lines',  on_delete=models.CASCADE)
+    journal = models.ForeignKey(Journal, related_name='accounting_lines',  on_delete=models.CASCADE)
     account_number = models.CharField(max_length=10)
     debit = models.DecimalField(max_digits=12, decimal_places=2)
     credit = models.DecimalField(max_digits=12, decimal_places=2)
+    
 
     def __str__(self):
         return self.account_number
